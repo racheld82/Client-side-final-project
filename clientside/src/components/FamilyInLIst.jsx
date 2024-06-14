74bff3c9d745655908498ade3e4a6159d9e7881d
רחלי דיסין <R0556724866@GMAIL.COM>
2024-06-14 12:30:43 +0300

    start

---
+++ clientside/src/components/FamilyInLIst.jsx
@@ -0,0 +1,16 @@
+import { useEffect, useState } from 'react'
+import React from 'react';
+import { useRef } from 'react'
+import {
+    Link,
+  useNavigate
+} from "react-router-dom";
+
+export default function FamilyInList(){
+    return (
+        <>
+        <h3>This should contain a component with table of family in list entities</h3>
+        </>
+    )
+}
+
